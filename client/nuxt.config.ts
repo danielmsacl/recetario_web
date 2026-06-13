c340b883b7e5b7eaeb75fcbb731acbc07abdf2c4
danielmsacl <danielewksstzibnhe@gmail.com>
2026-06-12 23:35:26 -0400

    nuxtconfig

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -6,5 +6,16 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxt/image'
-  ]
+  ],
+  // Configuración para redirigir peticiones de la API en producción
+  routeRules: {
+    // Redirige todas las peticiones que comiencen con /api
+    '/api/**': {
+      proxy: 'https://web-production-ccf1.up.railway.app/api/**'
+    },
+    // Redirige todas las peticiones de archivos subidos
+    '/uploads/**': {
+      proxy: 'https://web-production-ccf1.up.railway.app/uploads/**'
+    }
+  }
 })
