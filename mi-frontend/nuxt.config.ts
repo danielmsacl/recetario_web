7a859aeb826bd6786b19dd0ff39b5ad0239d7502
danielmsacl <danielewksstzibnhe@gmail.com>
2026-06-12 23:28:24 -0400

    update nuxt

--- mi-frontend/nuxt.config.ts
+++ mi-frontend/nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['nuxt-icons', '@nuxtjs/tailwindcss', '@nuxt/ui']
+  modules: ['nuxt-icons', '@nuxtjs/tailwindcss', '@nuxt/ui'],
+  routeRules: {
+    '/api/**': {
+      proxy: 'https://web-production-ccf1.up.railway.app/api/**'
+    },
+    '/uploads/**': {
+      proxy: 'https://web-production-ccf1.up.railway.app/uploads/**'
+    }
+  }
 })
